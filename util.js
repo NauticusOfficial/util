5bc850a5d56e3fd49564e6a29327f3a0f422441f
Alex <aredwood@users.noreply.github.com>
2018-04-24 20:40:18 +1000

    Update util.js

--- util.js
+++ util.js
@@ -16,3 +16,56 @@ function getParam(p,url){
     });
     return (found === "") ? false : (found === undefined) ? true : found;
 }
+
+/**
+ * @description gets the cookie given a key.
+ * @param {string} key - the key of the cookie to get.
+ * @return {string} value - the value of the cookie, null if not found.
+ * @author Alex Redwood
+ */
+function getCookie(key) {
+    var v = null ;
+    var cookies = document.cookie.split(";");
+    cookies.forEach(function(kv) {
+        var k_v = kv.split("=");
+        if (k_v[0].trim() === key) {
+            v = k_v[1];
+        }
+    });
+    return v;
+}
+
+/**
+ * @description sets a cookie, fairly simple.
+ * @param {string} name - the name of the cookie, could also be referred to as key.
+ * @param {*} value - the value of the cookie, will be converted into string.
+ * @param {object} options - alter the setting of a cookie, not used.
+ * @return {boolean} success - always true.
+ */
+function setCookie(name,value,options){
+
+    value = (typeof value === "string") ? value : (typeof value === "object") ? JSON.stringify(value) : value.toString();
+    var str = name + "=" + value + ";"
+    var location = "." + window.location.hostname;
+    // var str = `${name}=${value};`;
+    var defaults = {
+        "domain": location,
+        "secure":""
+    };
+
+    if(window.location.hostname === "localhost"){
+        delete defaults["domain"];
+        delete defaults["secure"]
+    }
+
+    for (var i in defaults) {
+        str += ";"+ i;
+    }
+    // Object.keys(defaults).forEach(def => {
+    //     str += `${def}${(defaults[def]) ? "="+defaults[def] : ""};`;
+    // });
+
+    document.cookie = str;
+    return true;
+
+}
